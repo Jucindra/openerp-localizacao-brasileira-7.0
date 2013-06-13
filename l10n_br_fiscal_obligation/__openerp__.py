e171f73bb4f22d8426a4e638234009a2e004957e
root <luiz.ferreira@monocon.com.br>
2013-06-13 16:56:57 -0300

    Adicionado l10n_br_fiscal_obligation

---
+++ l10n_br_fiscal_obligation/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Obrigações fiscais",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Danimar Ribeiro",
+    'category': 'Localisation Brasil',
+    'license': 'AGPL-3',
+    'website': 'http://www.openerpbrasil.org',
+    "description": """
+      Este módulo permite a geração das obrigações fiscais da localização brasileira.
+    """,
+    'depends': [
+        'l10n_br_base',
+        'account_asset',
+    ],
+    "init_xml": [],
+    'update_xml': ['l10n_br_fiscal_obligation.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
